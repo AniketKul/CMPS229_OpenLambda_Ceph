3afb8f9ffd6e67e839e498bbc215ca66527cea94
Ivo Jimenez <ivo.jimenez@gmail.com>
2017-05-16 22:52:44 -0700

    Runs container as daemon

--- experiments/single-node/run.sh
+++ experiments/single-node/run.sh
@@ -4,7 +4,7 @@ set -ex
 CEPH_PUBLIC_NETWORK=128.114.53.0/24
 MON_IP=128.114.53.161
 
-docker run --rm --name=cephdaemon \
+docker run -d --name=cephdaemon \
   --net=host \
   -e CEPH_PUBLIC_NETWORK=$CEPH_PUBLIC_NETWORK \
   -e MON_IP=$MON_IP \
